80109a24883a9a9c9cd5787c548ac3d27708846d
Henry Gressmann <mail@henrygressmann.de>
2023-02-18 23:10:09 +0100

    use correct font
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./global.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+	fonts: {
+		heading: `'Inter', sans-serif`,
+		body: `'Inter', sans-serif`,
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>,
