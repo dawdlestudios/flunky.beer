f3fa67f8bc844f1b30db1f9ffa5929fb9384e7cb
Henry Gressmann <mail@henrygressmann.de>
2023-02-18 14:13:59 +0100

    add run-all
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,6 @@ const InboxPage = ({}: RouteComponentProps<DefaultParams>) => {
 };
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
       <div>
         <Route path="/cock">
